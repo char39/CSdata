d979214e60f1109803d1c93e82d98d6a538b8a23
seongsun8330 <seongsun8330@naver.com>
2024-06-27 15:17:45 +0900

    .

---
+++ CsharpMidTest/CsharpMidTest/MainApp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpMidTest
+{
+    internal class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Random Ran = new Random();
+            int[] Lotto = { 1, 1, 1, 1, 1, 1 };
+
+            for (int i = 0; i < Lotto.Length; i++)
+            {
+                if (i == 0)
+                {
+                    Lotto[i] = Ran.Next(1, 46);
+                }
+                else
+                {
+                    Lotto[i] = Ran.Next(1, 46);
+                    for (int j = 0; j < i; j++)
+                    {
+                        while (Lotto[i] == Lotto[j])
+                            Lotto[i] = Ran.Next(1, 46);
+                    }
+                }
+            }
+            for (int i = 0; i < Lotto.Length; i++)
+                Console.WriteLine(Lotto[i]);
+        }
+    }
+}
---
+++ CsharpMidTest/CsharpMidTest/Program.cs
@@ -0,0 +1,94 @@
+﻿//using System;
+
+
+
+//namespace CsharpMidTest
+//{
+//    public class A
+//    {
+//        public string[] Day = { "월", "화", "수", "목", "금", "토", "일" };
+//        static public int Sum(int x, int y)
+//        {
+//            return x + y;
+//        }
+//        public void Inst()
+//        {
+//            for (int i = 0; i < Day.Length; i++)
+//                Console.WriteLine(Day[i]);
+//        }
+//    }
+//    public class B
+//    {
+//        static public void Method()
+//        {
+//            int temp = A.Sum(5, 6);
+//            Console.WriteLine($"더하기는 : {temp}");
+//        }
+//        static public void Method2()
+//        {
+//            A func = new A();
+//            func.Inst();
+//        }
+//    }
+//    public class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int num1 = 5, num2 = 6;
+//            B.Method();
+//            B.Method2();
+
+//            #region
+//            /*
+//            6개 번호를 출력하는데, 중복이 되면안됨
+//            1부터 45번까지 의 랜덤번호
+
+            
+//             */
+//            //Random Ran = new Random();
+//            //int[] Lotto = {1,2,3,4,5,6};
+//            //for (int i = 0; i < 6; i++)
+//            //{
+//            //    foreach (int j in Lotto)
+//            //    {
+//            //        if (i != 0)
+//            //        {
+//            //            if (j == Lotto[i])
+//            //            {
+//            //                i--;
+//            //                break;
+//            //            }
+//            //        }
+//            //        //if ((j == Lotto[i]) && !(i==0))
+//            //        //{
+//            //        //    break;
+//            //        //}
+//            //        //else
+//            //        //{
+//            //        //    break;
+//            //        //}
+//            //    }
+
+//            //    Lotto[i] = Ran.Next(1, 46);
+//            //    Console.WriteLine(Lotto[i]);
+
+//            //}
+
+//            //j랑 로또랑 같고, i가 0일때
+
+//            //Random Ran = new Random();
+//            //int[] Lotto = { 1, 2, 3, 4, 5, 6 };
+//            //for (int i = 0; i < Lotto.Length; i++)
+//            //{
+
+//            //}
+
+//            //void GiveNum()
+//            //{
+//            //    Lotto[i] = Ran.Next(1, 46);
+//            //}
+//            //Console.WriteLine($"더하기는 : {A.Sum(num1, num2)}");
+//            #endregion
+//        }
+//    }
+//}
