9c9aba04f751eac850f4117a75ceada7656b807b
seongsun8330 <seongsun8330@naver.com>
2024-07-02 17:15:55 +0900

    0702_2 쪽지시험 완료 후 커밋

--- Csharp07_02NETCORE/Csharp07_02NETCORE/MainApp2.cs
+++ Csharp07_02NETCORE/Csharp07_02NETCORE/MainApp2.cs
@@ -1,70 +1,70 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Diagnostics;
-using System.Threading.Channels;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Diagnostics;
+//using System.Threading.Channels;
 
-namespace Csharp07_02NETCORE
-{
-    #region P316
-    //internal class EnvironmentExit
-    //{
-    //    static void Main()
-    //    {
-    //        Console.WriteLine("출력됩니다.");
-    //        Environment.Exit(0);
-    //        Console.WriteLine("출력될까요?");
-    //    }
-    //}
-    #endregion
-    #region P318 exe파일 실행하기
+//namespace Csharp07_02NETCORE
+//{
+//    #region P316
+//    //internal class EnvironmentExit
+//    //{
+//    //    static void Main()
+//    //    {
+//    //        Console.WriteLine("출력됩니다.");
+//    //        Environment.Exit(0);
+//    //        Console.WriteLine("출력될까요?");
+//    //    }
+//    //}
+//    #endregion
+//    #region P318 exe파일 실행하기
 
-    //class ProcessStartDemo
-    //{
-    //    static void Main()
-    //    {
-    //        Process.Start("Notepad.exe");
-    //        Process.Start("Explorer.exe", "http://dotnetkorea.com");
-    //    }
-    //}
+//    //class ProcessStartDemo
+//    //{
+//    //    static void Main()
+//    //    {
+//    //        Process.Start("Notepad.exe");
+//    //        Process.Start("Explorer.exe", "http://dotnetkorea.com");
+//    //    }
+//    //}
 
-    #endregion
-    #region P320 로또번호 생성기
+//    #endregion
+//    #region P320 로또번호 생성기
 
-    class RandomDemo
-    {
-        static void Main()
-        {
-            Console.Write("이번 주의 로또번호 : ");
-            Random ran = new Random();
-            int[] arr = new int[6];
-            int temp = 0;
-            for (int i = 0; i < arr.Length; i++)
-            {
-                temp = ran.Next(1, 46);
-                bool flag = false;
-                if (i > 0 && i < 6)
-                {
-                    for (int j = 0; j <= i; j++)
-                    {
-                        if (arr[j] == temp)
-                        {
-                            flag = true;
-                        }
-                    }
-                }
-                if (flag)
-                    --i;
-                else
-                    arr[i] = temp;
-            }
-            for (int i = 0;i < arr.Length; i++)
-                Console.WriteLine($"{arr[i]}, ");
-            Console.WriteLine();
-        }
-    }
+//    class RandomDemo
+//    {
+//        static void Main()
+//        {
+//            Console.Write("이번 주의 로또번호 : ");
+//            Random ran = new Random();
+//            int[] arr = new int[6];
+//            int temp = 0;
+//            for (int i = 0; i < arr.Length; i++)
+//            {
+//                temp = ran.Next(1, 46);
+//                bool flag = false;
+//                if (i > 0 && i < 6)
+//                {
+//                    for (int j = 0; j <= i; j++)
+//                    {
+//                        if (arr[j] == temp)
+//                        {
+//                            flag = true;
+//                        }
+//                    }
+//                }
+//                if (flag)
+//                    --i;
+//                else
+//                    arr[i] = temp;
+//            }
+//            for (int i = 0;i < arr.Length; i++)
+//                Console.WriteLine($"{arr[i]}, ");
+//            Console.WriteLine();
+//        }
+//    }
 
-    #endregion
-}
+//    #endregion
+//}
---
+++ Csharp07_02NETCORE/Csharp07_02NETCORE/Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp07_02NETCORE
+{
+    internal class Test
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
