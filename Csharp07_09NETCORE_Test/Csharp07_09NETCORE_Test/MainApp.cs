0aec0ed2dfbda2301c7fba03d8b3ce122a3d0cfd
seongsun8330 <seongsun8330@naver.com>
2024-07-09 17:35:36 +0900

    0709_C#테스트 후

---
+++ Csharp07_09NETCORE_Test/Csharp07_09NETCORE_Test/MainApp.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp07_09NETCORE_Test
+{
+    #region original
+    //struct ACSetting
+    //{
+    //    public double currentInCelsius;
+    //    public double target;
+
+    //    public readonly double GetFahrenheit()
+    //    {
+    //        target = currentInCelsius * 1.8 + 32;
+    //        return target;
+    //    }
+
+    //}
+    //public class MainApp
+    //{
+    //    static void Main()
+    //    {
+    //        ACSetting acs;
+    //        acs.currentInCelsius = 25;
+    //        acs.target = 25;
+
+    //        Console.WriteLine($"{acs.GetFahrenheit()}");
+    //        Console.WriteLine($"{acs.target}");
+    //    }
+    //}
+
+    #endregion
+    #region just remove
+    struct ACSetting
+    {
+        public double currentInCelsius;
+        public double target;
+
+        public readonly double GetFahrenheit()
+        {
+            return currentInCelsius * 1.8 + 32;
+        }
+
+    }
+    public class MainApp
+    {
+        static void Main()
+        {
+            ACSetting acs;
+            acs.currentInCelsius = 25;
+            acs.target = 25;
+
+            Console.WriteLine($"{acs.GetFahrenheit()}");
+            Console.WriteLine($"{acs.target}");
+        }
+    }
+    #endregion
+
+
+}
---
+++ Csharp07_09NETCORE_Test/Csharp07_09NETCORE_Test/Program.cs
@@ -0,0 +1,10 @@
+﻿// namespace Csharp07_09NETCORE_Test
+// {
+//     internal class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             Console.WriteLine("Hello, World!");
+//         }
+//     }
+// }
