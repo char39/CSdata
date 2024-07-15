77029838c5ff39c37025d4c943fb7bb4911b89ce
Your Name <seongsun8330@naver.com>
2024-07-15 23:43:39 +0900

    0715_함수 작성완료...

--- ☆수업 내용정리☆/C# .NET 8.0/C#/CSharp.cs
+++ ☆수업 내용정리☆/C# .NET 8.0/C#/CSharp.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Net;
 
 /*  ★ static void Main() ★
 namespace CSharp
@@ -26,7 +25,7 @@ namespace CSharp
     - Main() 메서드는 프로그램 내에 단 하나만 존재해야 함
 */
 
-#region ★ C# 기본 ★
+#region ★ C# 정리 ★   」(￣▽￣」)
 
 #region 1. Console.WriteLine() [p64~67]
 
@@ -578,13 +577,336 @@ namespace CSharp
 */
 
 #endregion
-#region 8.
+#region 8. 함수 (Function) [p243~271]   ⎛⎝。O ⩊ O。⎛⎝
 
+/*  8.1 함수란 :
+        프로그램의 기능을 수행하는 코드 블록. 재사용성을 높이고 코드의 중복을 줄임.
+        함수는 메서드(Method)라고도 함.
 
+        8.1.1 함수의 실행 단계 :
+            1. 함수 호출, 2. 함수 정의, 3. 함수 실행, 4. 함수 반환
+
+        8.1.2 내장 함수, 사용자 정의 함수 :
+            내장 함수 : .NET Framework에 미리 정의된 함수. API(Application Programming Interface) 제공
+            사용자 정의 함수 : 사용자가 직접 정의한 함수
+
+    8.2 함수의 사용법 :
+
+        8.2.1 함수의 정의 형식 :
+            [접근제한자] [static] 반환형식 함수명(매개변수)
+            {
+                명령문;
+                return 반환값;
+            }
+
+        8.2.2 함수의 정의 예시 :
+            public int Add(int a, int b)    // 접근제한자 public, 반환형식 int, 함수명 Add, 매개변수 a, b
+            {
+                return a + b;               // 반환값 a + b
+            }
+        
+        8.2.3 함수의 호출 형식 :
+            함수명(매개변수);
+        
+        8.2.4 함수의 호출 예시 :
+            int result = Add(10, 20);      // Add 함수 호출, 매개변수 10, 20 전달
+
+        8.2.5 XML 주석 :
+            함수 위에 XML 주석을 작성하여 함수의 설명, 매개변수, 반환값을 기술
+            함수를 먼저 작성하고, 함수명 위에 ///을 입력하면 자동으로 XML 주석이 생성
+            /// <summary>
+            /// 함수의 설명
+            /// </summary>
+            /// <param name="매개변수">매개변수 설명</param>
+            /// <returns>반환값 설명</returns>
+
+    8.3 매개변수(Parameter), 반환값(Return Value) :
+            
+        8.3.1 매개변수(Parameter) :
+            함수를 호출할 때 전달하는 값
+            매개변수는 함수의 정의에 선언된 변수
+
+            8.3.1.1 인자, 파라미터 :
+                인자(Argument) : 함수를 호출할 때 전달하는 값
+                파라미터(Parameter) : 함수를 정의할 때 선언하는 변수
+
+        8.3.2 반환값(Return Value) :
+            함수의 실행 결과를 반환
+            반환값은 함수의 정의에 선언된 반환형식과 일치해야 함
+    
+        8.3.3 예시 :
+            internal class Program
+            {
+                static void Main()                  // (1) Main 함수 정의, void는 반환값이 없음
+                {
+                    int result = Add(10, 20);       // (2) Add 함수 호출, 인자 10, 20 전달
+                    Console.WriteLine(result);      // (5) Add 함수 반환값 콘솔에 출력, (6) 종료
+                }
+                int Add(int a, int b)               // (3) Add 함수 정의, 매개변수 a, b
+                {
+                    return a + b;                   // (4) a + b 반환
+                }
+            }
+
+    8.4 매개변수 전달 방식 :
+    
+        8.4.1 값에 의한 전달(Call by Value) :
+            매개변수로 전달된 값을 복사하여 전달
+            함수 내에서 매개변수 값을 변경해도 호출자에게 영향을 주지 않음
+
+        8.4.2 참조에 의한 전달(Call by Reference) :
+            매개변수로 전달된 값을 참조하여 전달
+            함수 내에서 매개변수 값을 변경하면 호출자에게 영향을 줌
+
+            8.4.2.1 ref 키워드 :
+                매개변수 앞에 명시적으로 ref 키워드를 사용하여 참조에 의한 전달을 지정
+                ref 키워드를 사용하였을 때 함수 내에서 매개변수 값을 변경하면 호출자에게 영향을 줌
+                다만 매개변수로 전달할 때 반드시 초기화되어 있어야 함
+            
+            8.4.2.2 out 키워드 :
+                매개변수 앞에 명시적으로 out 키워드를 사용하여 참조에 의한 전달을 지정
+                out 키워드를 사용하였을 때 함수 내에서 매개변수 값을 변경하면 호출자에게 영향을 줌
+                매개변수로 전달할 때 초기화되어 있지 않아도 되나, 함수 내부에서 반드시 초기화되어야 함
+
+            8.4.2.3 각각 장점 :
+                ref : 
+                    메서드 내에서 전달하는 변수를 변경하고자 할 때 사용
+                    매개변수에 직접 접근하여 값의 복사가 일어나는 것을 방지하기 때문에 성능 향상을 기대할 수 있음
+                    즉 시간과 메모리를 절약할 수 있음
+                out : 
+                    메서드 내에서 전달하는 변수를 초기화하고자 할 때 사용
+                    매서드 내에서 반드시 할당해야 하기 때문에 프로그래머의 실수를 방지할 수 있음
+
+        8.4.3 예시 :
+            return을 사용하여 값을 반환하는 것이 아닌, ref, out을 사용하여 값에 직접 접근하여 값을 변경하는 것
+            
+            8.4.3.1 값에 의한 전달(Call by Value) :
+                internal class Program
+                {
+                    static void Main()
+                    {
+                        int var = 1;                            // (1) 변수 var 선언, 초기화
+                        ValueFunction(var);                     // (2) ValueFunction 함수 호출, var 값 전달
+                        Console.WriteLine(var);                 // (5) var 값 = 1 출력, (6) 종료
+                    }
+                    public static void ValueFunction(int i)     // (3) ValueFunction 함수 정의, 매개변수 i 선언
+                    {
+                        i = 10;                                 // (4) i 값 변경. 호출자에게 영향을 주지 않음
+                    }
+                }
+            
+            8.4.3.2 참조(ref)에 의한 전달(Call by Reference) :
+                internal class Program
+                {
+                    static void Main()
+                    {
+                        int var = 1;                            // (1) 변수 var 선언, 초기화
+                        RefFunction(ref var);                   // (2) RefFunction 함수 호출, var 값의 참조 전달
+                        Console.WriteLine(var);                 // (5) var 값 = 10 출력, (6) 종료
+                    }
+                    public static void RefFunction(ref int i)   // (3) RefFunction 함수 정의, 매개변수 i 선언
+                    {
+                        i = 10;                                 // (4) i 값 변경. 호출자에게 영향을 줌 {참조된 변수의 값이 변경됨} 초기화가 필수는 아님
+                    }
+                }
+            
+            8.4.3.3 참조(out)에 의한 전달(Call by Reference) :
+                internal class Program
+                {
+                    static void Main()
+                    {
+                        int var;                                // (1) 변수 var 선언
+                        OutFunction(out var);                   // (2) OutFunction 함수 호출, var 값의 참조 전달
+                        Console.WriteLine(var);                 // (5) var 값 = 10 출력, (6) 종료
+                    }
+                    public static void OutFunction(out int i)   // (3) OutFunction 함수 정의, 매개변수 i 선언
+                    {
+                        i = 10;                                 // (4) i 값 변경. 호출자에게 영향을 줌 {참조된 변수의 값이 변경됨} 초기화가 필수
+                    }
+                }
+    
+    8.5 명명된 매개변수 :
+        매개변수의 이름을 명시하여 전달하는 방식
+        매개변수의 순서를 지키지 않고 전달 가능
+
+        8.5.1 명명된 매개변수 예시 :
+            internal class Program
+            {
+                static void Main()
+                {
+                    NamedFunction(b: 10, a: 20);                // (1) NamedFunction 함수 호출, 명명된 매개변수 a, b 전달
+                }
+                public static void NamedFunction(int a, int b)  // (2) NamedFunction 함수 정의, 매개변수 a, b 선언
+                {
+                    Console.WriteLine(a);                       // (3) a 값 = 20 출력
+                    Console.WriteLine(b);                       // (4) b 값 = 10 출력
+                }
+            }
+
+    8.6 함수 오버로드 (다중 정의) :
+        동일한 이름의 함수를 여러 개 정의하는 것. 매개변수의 개수, 자료형, 순서가 다르면 가능
+        
+        8.6.1 매개변수가 없거나 있는 함수 오버로드 :
+            internal class Program
+            {
+                static void Main()
+                {
+                    OverloadFunction();                         // (1) OverloadFunction 함수 호출
+                    OverloadFunction(10);                       // (4) OverloadFunction 함수 호출, 매개변수 10 전달
+                }
+                public static void OverloadFunction()           // (2) OverloadFunction 함수 정의, 매개변수 없음
+                {
+                    Console.WriteLine("매개변수 없음");          // (3) (매개변수 없음) 출력
+                }
+                public static void OverloadFunction(int i)      // (5) OverloadFunction 함수 정의, 매개변수 i 선언
+                {
+                    Console.WriteLine("매개변수 : " + i);       // (6) (매개변수 : 10) 출력
+                }
+            }
+
+        8.6.2 매개변수의 자료형이 다른 함수 오버로드 :
+            internal class Program
+            {
+                static void Main()
+                {
+                    OverloadFunction(10);                       // (1) OverloadFunction 함수 호출, 매개변수 10 전달
+                    OverloadFunction(10.5);                     // (4) OverloadFunction 함수 호출, 매개변수 10.5 전달
+                }
+                public static void OverloadFunction(int i)      // (2) OverloadFunction 함수 정의, 매개변수 i 선언
+                {
+                    Console.WriteLine("정수 : " + i);           // (3) (정수 : 10) 출력
+                }
+                public static void OverloadFunction(double d)   // (5) OverloadFunction 함수 정의, 매개변수 d 선언
+                {
+                    Console.WriteLine("실수 : " + d);           // (6) (실수 : 10.5) 출력
+                }
+            }
+
+        8.6.3 매개변수의 순서가 다른 함수 오버로드 :
+            internal class Program
+            {
+                static void Main()
+                {
+                    OverloadFunction(10, 20.5);                     // (1) OverloadFunction 함수 호출, 매개변수 10, 20.5 전달
+                    OverloadFunction(20.5, 10);                     // (4) OverloadFunction 함수 호출, 매개변수 20.5, 10 전달
+                }
+                public static void OverloadFunction(int a, float b) // (2) OverloadFunction 함수 정의, 매개변수 a, b 선언
+                {
+                    Console.WriteLine("a : " + a + ", b : " + b);   // (3) (a : 10, b : 20.5) 출력
+                }
+                public static void OverloadFunction(float b, int a) // (5) OverloadFunction 함수 정의, 매개변수 b, a 선언
+                {
+                    Console.WriteLine("b : " + b + ", a : " + a);   // (6) (b : 20.5, a : 10) 출력
+                }
+            }
+
+    8.7 가변 길이 매개변수 :
+        매개변수의 개수가 가변적인 함수 정의
+        params 키워드를 사용하여 가변 길이 매개변수를 지정
+
+        8.7.1 가변 길이 매개변수 예시 :
+            internal class Program
+            {
+                static void Main()
+                {
+                    Console.WriteLine(Sum(1, 2, 3, 4, 5));          // Sum 함수 호출, 매개변수 1, 2, 3, 4, 5 전달
+                    Console.WriteLine(Sum(1, 2, 3, 4, 5, 6, 7));    // Sum 함수 호출, 매개변수 1, 2, 3, 4, 5, 6, 7 전달
+                }
+
+                public static int Sum(params int[] numbers)         // 매개변수를 전부 더하여 반환하는 Sum 함수
+                {
+                    int sum = 0;
+                    foreach (int i in numbers)
+                    {
+                        sum += i;
+                    }
+                    return sum;
+                }
+            }
+
+    8.8 재귀 함수 :
+        함수 내에서 자기 자신을 호출하는 함수
+        재귀 함수는 반드시 종료 조건을 설정해야 함
+
+        8.8.1 재귀 함수 예시 :
+            internal class Program
+            {
+                static void Main()
+                {
+                    Console.WriteLine(Factorial(5));                // Factorial 함수 호출, 매개변수 5 전달
+                }
+
+                public static int Factorial(int i)                  // 팩토리얼을 구하는 Factorial 함수
+                {
+                    if (i == 1) return 1;                           // 종료 조건. i가 1이면 1 반환하고 재귀함수를 빠져나감
+                    return i * Factorial(i - 1);                    // 자기 자신을 계속 호출하여 계속된 반환값을 곱함. ex) 5 * 4 * 3 * 2 * 1
+                }
+            }
+    
+    8.9 함수 범위 :
+        함수 내에서 선언된 변수는 함수 내에서만 사용 가능
+        함수 외부에서 선언된 변수(클래스 내에서 선언된 변수)는 함수 내부에서 사용 가능
+
+        8.9.1 함수 범위 예시 :
+            public class Program
+            {
+                public static int globalvar = 100;      // (1) 전역변수 globalvar 선언, 초기화. [정적 할당]
+
+                public static void Main()               // (2) Main 함수 정의
+                {
+                    Console.WriteLine(globalvar);       // (3) globalvar 값 = 100 출력
+                    Console.WriteLine(localvar);        // (4) ★ 오류 발생
+                    Program program = new Program();    // (5) Program 클래스의 program 객체 생성. [동적 할당]
+                    program.Function();                 // (6) program 객체의 Function 함수 호출.
+                }
+
+                public void Function()                  // (7) Function 함수 정의
+                {
+                    int localvar = 10;                  // (8) 지역변수 localvar 선언, 초기화
+                    Console.WriteLine(localvar);        // (9) localvar 값 = 10 출력
+                }
+            }
+
+    8.10 로컬 함수 :
+        함수 내부에 선언된 함수
+        로컬 함수는 함수 내부에서만 사용 가능
+
+        8.10.1 로컬 함수 예시 :
+            internal class Program
+            {
+                static void Main()                          // (1) Main() 메서드 정의
+                {
+                    LocalFunction();                        // (2) LocalFunction() 함수 호출
+                    void LocalFunction()                    // (3) LocalFunction() 함수 정의
+                    {
+                        Console.WriteLine("로컬 함수");      // (4) "로컬 함수" 출력
+                    }
+                }
+            }
+    
+    8.11 Main() 메서드의 명령줄 인수 :
+        Main() 메서드의 매개변수로 string[] args를 사용하여 명령줄 인수를 전달
+        명령줄 인수는 프로그램 실행 시 전달하는 인수
+
+        8.11.1 Main() 메서드의 명령줄 인수 예시 :
+            internal class Program
+            {
+                static void Main(string[] args)              // (1) Main() 메서드 정의, 매개변수 args 선언
+                {
+                    foreach (string arg in args)             // (2) args 배열의 요소를 반복하여 출력
+                    {
+                        Console.WriteLine(arg);               // (3) args 배열의 요소 출력
+                    }
+                }
+            }
+        
+        8.11.2 명령줄 인수 입력 방법 :
+            1. 프로젝트 속성 > 디버그 > 명령줄 인수 입력
+            2. 프로그램 실행 시 명령줄에 인수 입력
+
+*/
 
 #endregion
-
-
 
 
 
