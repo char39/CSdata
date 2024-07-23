2d3184bc7d4a5fc097f4e63aa08dc61e2bb49d48
seongsun8330 <seongsun8330@naver.com>
2024-07-23 13:53:39 +0900

    0723_C#생성

---
+++ 202407/Csharp07_23NETCORE/Csharp07_23NETCORE/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Csharp07_23NETCORE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
