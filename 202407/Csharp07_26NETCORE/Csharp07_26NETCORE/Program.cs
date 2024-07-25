bb184125fe00d653baa0a1565f0db1f295874a75
Cinnamon <seongsun8330@naver.com>
2024-07-26 08:52:51 +0900

    0726_솔루션추가

---
+++ 202407/Csharp07_26NETCORE/Csharp07_26NETCORE/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Csharp07_26NETCORE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
