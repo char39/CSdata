a0ee3b228370095d6a69d52ddecd2a25acdaba03
Cinnamon <seongsun8330@naver.com>
2024-08-07 09:07:23 +0900

    .

---
+++ 202408/Csharp08_07NETCORE/Csharp08_07NETCORE/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Csharp08_07NETCORE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
