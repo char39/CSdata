702d5fecb81b31afdaae6bfdef4cf089bd85d014
Cinnamon <seongsun8330@naver.com>
2024-08-08 14:31:38 +0900

    0808_엑셀참조

--- 202408/Csharp08_08NETCORE/Csharp08_08/Program.cs
+++ 202408/Csharp08_08NETCORE/Csharp08_08/Program.cs
@@ -1,10 +1,88 @@
-﻿namespace Csharp08_08
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp08_08
 {
-    internal class Program
+/*  dynamic 예시
+    class MyClass
     {
-        static void Main(string[] args)
+        public void FuncA()
         {
-            Console.WriteLine("Hello, World!");
+
         }
     }
+
+    internal class Program
+    {
+        static void Main()
+        {
+            dynamic obj = new MyClass();    // dynamic 키워드로 선언된 obj는 컴파일 시점에 MyClass의 FuncA()만 인식
+            obj.FuncA();
+            obj.FuncB();
+        }
+    } */
+/*  dynamin 예시 2
+    class Duck
+    {
+        public void Walk()
+        {
+            Console.WriteLine("Duck Walk");
+        }
+        public void Swim()
+        {
+            Console.WriteLine("Duck Swim");
+        }
+        public void Say()
+        {
+            Console.WriteLine("Duck Say");
+        }
+    }
+
+    class Mallard : Duck {}
+    class Robot
+    {
+        public void Walk()
+        {
+            Console.WriteLine("Robot Walk");
+        }
+        public void Swim()
+        {
+            Console.WriteLine("Robot Swim");
+        }
+        public void Say()
+        {
+            Console.WriteLine("Robot Say");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine();
+
+            dynamic[] arr = [new Duck(), new Mallard(), new Robot()];
+            foreach (dynamic duck in arr)
+            {
+                Console.WriteLine(duck.GetType());
+                duck.Walk();
+                duck.Swim();
+                duck.Say();
+                Console.WriteLine();
+            }
+        }
+    } */
+
+
+    internal class Program
+    {
+        static void Main()
+        {
+            
+        }
+    }
+
 }
