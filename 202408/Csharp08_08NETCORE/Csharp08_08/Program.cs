b985565d21e194badc17d65b17326a29e5ddae1b
Cinnamon <seongsun8330@naver.com>
2024-08-08 14:39:15 +0900

    0808_윈도우에서 엑셀참조 해야됨

--- 202408/Csharp08_08NETCORE/Csharp08_08/Program.cs
+++ 202408/Csharp08_08NETCORE/Csharp08_08/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace Csharp08_08
 {
@@ -79,9 +80,15 @@ namespace Csharp08_08
 
     internal class Program
     {
+        public static void OldWay(string[] data, string savePath)
+        {
+            Excel.Application excelApp = new Excel().Application();
+            excelApp.Workbooks.Add(Type.Missing);
+        }
+
         static void Main()
         {
-            
+
         }
     }
 
