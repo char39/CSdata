69011861bbc6b5fe99abf3935648646fa991784f
Cinnamon <seongsun8330@naver.com>
2024-08-13 12:05:35 +0900

    0813_파일추가

---
+++ 202408/Csharp08_13NETCORE/Csharp08_13NETCORE/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Csharp08_13NETCORE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
