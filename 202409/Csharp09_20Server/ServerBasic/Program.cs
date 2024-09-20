5af22bb3723f2dca2f8119fc91c14eca8f720c68
Cinnamon <seongsun8330@naver.com>
2024-09-20 11:33:31 +0900

    .

--- 202409/Csharp09_20Server/ServerBasic/Program.cs
+++ 202409/Csharp09_20Server/ServerBasic/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace ServerBasic
+﻿using System;
+using System.Threading;
+
+namespace ServerBasic
 {
     internal class Program
     {
+        static void MainThread()
+        {
+            while (true)
+            {
+                Console.WriteLine("ServerBasic.Program.MainThread()");
+                Thread.Sleep(100);
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Server!");
+            Thread thread = new(MainThread);
+            thread.IsBackground = true;
+            thread.Start();     // Start는 새로운 스레드를 만들어서 MainThread()를 실행
+            Console.WriteLine("Waiting for MainThread to finish");
+            thread.Join();      // Join은 MainThread()가 끝날 때까지 기다림
+            Console.WriteLine("Hello, Server Basic!");
         }
     }
 }
