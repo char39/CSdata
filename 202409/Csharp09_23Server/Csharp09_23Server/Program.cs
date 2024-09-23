96f271dba96f8aed679c37c4d9a28e2ae2bc30fb
Cinnamon <seongsun8330@naver.com>
2024-09-23 09:25:50 +0900

    .

---
+++ 202409/Csharp09_23Server/Csharp09_23Server/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Csharp09_23Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
