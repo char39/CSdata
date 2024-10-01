0790035106b829ca295d5c8b412963bdfe506c89
Cinnamon <seongsun8330@naver.com>
2024-10-01 09:59:14 +0900

    1001_Session.cs 추가. Socket 배우는중

---
+++ 202410/Csharp10_01Server/ServerCore/Listener.cs
@@ -0,0 +1,45 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ServerCore
+{
+    public class Listener
+    {
+        private Socket listenSocket;
+        private Action<Socket> OnAcceptHandler;
+        private SocketAsyncEventArgs recvArgs = new();
+
+        public void Initialize(IPEndPoint endPoint, Action<Socket> OnAcceptHandler)
+        {
+            listenSocket = new(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            this.OnAcceptHandler += OnAcceptHandler;
+
+            listenSocket.Bind(endPoint);
+            listenSocket.Listen(10);
+
+            recvArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnAcceptCompleted);
+            RegisterAccept(recvArgs);
+        }
+
+        private void RegisterAccept(SocketAsyncEventArgs args)
+        {
+            args.AcceptSocket = null;
+
+            bool pending = listenSocket.AcceptAsync(args);      // 보류 중인가
+            if (!pending)
+                OnAcceptCompleted(null, args);
+        }
+
+        private void OnAcceptCompleted(object sender, SocketAsyncEventArgs args)
+        {
+            if (args.SocketError == SocketError.Success)
+                OnAcceptHandler.Invoke(args.AcceptSocket);
+            else
+                Console.WriteLine(args.SocketError.ToString());
+
+            RegisterAccept(args);
+        }
+    }
+}
---
+++ 202410/Csharp10_01Server/ServerCore/Session.cs
@@ -0,0 +1,64 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace ServerCore
+{
+    public class Session
+    {
+        Socket socket;
+        public void Initialize(Socket socket)
+        {
+            this.socket = socket;
+            SocketAsyncEventArgs recvArgs = new();
+            recvArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnReceiveCompleted);
+            recvArgs.SetBuffer(new byte[1024], 0, 1024);
+            RegisterReceive(recvArgs);
+        }
+
+        public void Send(byte[] sendBuff)
+        {
+            socket.Send(sendBuff);
+        }
+
+        public void Disconnect()
+        {
+            socket.Shutdown(SocketShutdown.Both);
+            socket.Close();
+        }
+
+
+        /// <summary> 네트워크 통신 메서드 </summary>
+        private void RegisterReceive(SocketAsyncEventArgs args)
+        {
+            bool pending = socket.ReceiveAsync(args);
+            if (!pending)
+                OnReceiveCompleted(null, args);
+
+        }
+        /// <summary> 네트워크 통신 메서드 </summary>
+        private void OnReceiveCompleted(object sender, SocketAsyncEventArgs args)
+        {
+            if (args.BytesTransferred > 0 && args.SocketError == SocketError.Success)
+            {
+                try
+                {
+                    string recvData = Encoding.UTF8.GetString(args.Buffer, args.Offset, args.BytesTransferred);
+                    Console.WriteLine($"From[Client] : {recvData}");
+                    RegisterReceive(args);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("OnReceievCompleted Failed : " + ex.ToString());
+                }
+            }
+            else
+            {
+
+            }
+        }
+
+
+
+
+    }
+}
